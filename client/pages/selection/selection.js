c1e294c8e4b80fd6e656fd80a6e9d0f6013ac5cd
owenzh <zhangxudong_sun@sina.com>
2018-07-06 10:09:11 +0800

    Display article list via category tab and move the style into app.wxss.

--- client/pages/selection/selection.js
+++ client/pages/selection/selection.js
@@ -1,3 +1,7 @@
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+var moment = require('../../vendor/moment/moment.js');
 var app = getApp()
 Page({
   data: {
@@ -5,18 +9,13 @@ Page({
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    var category = app.getCategory();
     this.setData({
-      cate: app.getCategory()
-    })
+      cate: category
+    });
+    this.getArticleViaCategory(category[0].value);
   },
-  //滑动切换
-  swiperTab: function (e) {
-    // console.log(e);
-    // var that = this;
-    // that.setData({
-    //   currentTab: e.detail.current
-    // });
-  },
+
   //点击切换
   clickTab: function (e) {
     var that = this;
@@ -25,8 +24,31 @@ Page({
     } else {
       that.setData({
         currentTab: e.target.dataset.current
-      })
+      });
+      that.getArticleViaCategory(e.target.dataset.currentCategory.value);
     }
-    console.log(that.data.currentTab)
-  }
+  },
+  getArticleViaCategory: function (categoryVal) {
+    util.showBusy('请求中...')
+    var that = this
+    qcloud.request({
+      url: `${config.service.host}/weapp/article_list/category/${categoryVal}`,
+      login: true,
+      success(result) {
+        util.showSuccess('请求成功完成')
+        var article_list = result.data.data.res;
+        for (var i = 0, len = article_list.length; i < len; i++) {
+          var dataIns = new Date(article_list[i].creatts);
+          article_list[i].creatts = moment(dataIns).format('YYYY-MM-DD HH:mm');
+        }
+        that.setData({
+          requestResult: article_list
+        })
+      },
+      fail(error) {
+        util.showModel('请求失败', error);
+        console.log('request fail', error);
+      }
+    })
+  },
 })
